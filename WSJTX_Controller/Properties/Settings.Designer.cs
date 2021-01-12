bbe14e48ff519ddf7de24e8e0e91384d8d551d1b
avantol <git.avantol@xoxy.net>
2021-01-12 09:59:02 -0700

    (build 68) Installer code-signed, default timeout now 4 Tx

--- WSJTX_Controller/Properties/Settings.Designer.cs
+++ WSJTX_Controller/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace WSJTX_Controller.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("6")]
+        [global::System.Configuration.DefaultSettingValueAttribute("4")]
         public int timeout {
             get {
                 return ((int)(this["timeout"]));
